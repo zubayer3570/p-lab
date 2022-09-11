b8821a579250e0a533fe17321acf9b403b5aef35
Mahamudul Hasan Zubayer <94388111+zubayer3570@users.noreply.github.com>
2022-09-11 19:30:25 +0600

    Update question-1.c

--- question-1.c
+++ question-1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     int n, k, result;
     printf("Enter Range and position: ");
     scanf("%d %d", &n, &k);
-    result = (k <= n / 2) ? (2 * k - 1) : (2 * (k - n / 2));
+    result = (k <= ceil(n/2) ) ? (2 * k - 1) : (2 * (k - n / 2));
     printf("number: %d", result);
 }
